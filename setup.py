4061d0c94cd8453f608fb53778f46fe086de89ca
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-05-09 22:35:21 +0200

    Add a setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    README = f.read()
+
+classifiers = ["Programming Language :: Python",
+               "License :: OSI Approved :: Apache Software License",
+               "Development Status :: 4 - Beta"]
+
+
+setup(name='cambouis',
+      version='0.0.1',
+      url='https://github.com/tOkeshu/cambouis',
+      packages=find_packages(),
+      long_description=README,
+      description="An IRC bot for La Quadrature Du Net",
+      author="Romain Gauthier",
+      author_email="romain.gauthier@monkeypatch.me",
+      include_package_data=True,
+      classifiers=classifiers)
